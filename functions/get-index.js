2cdaa540194e84f24dbbeebdbcaa89187f93f502
Avinash Yadav <avinash@Avinashs-MacBook-Air.local>
2018-10-24 14:56:45 +0530

    connected landing page with restaurant api video 2.3

--- functions/get-index.js
+++ functions/get-index.js
@@ -3,7 +3,11 @@
 const co =require('co');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
+const Mustache = require('mustache');
+const http = require('superagent-promise')(require('superagent'), Promise);
 
+const restaurantsApiRoot = process.env.restaurants_api;
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var html;
 
 function* loadHtml() {
@@ -13,9 +17,16 @@ function* loadHtml() {
   return yield fs.readFileAsync('static/index.html', 'utf-8'); 
 }
 
+function* getRestaurants() {
+  return (yield http.get(restaurantsApiRoot)).body;
+}
 
 module.exports.handler = co.wrap(function* (event, context, cb) {
-  let html = yield loadHtml();
+  let template = yield loadHtml();
+  let restaurants = yield getRestaurants();
+  let daysOfWeek = days[new Date().getDay()];
+  let html = Mustache.render(template, { daysOfWeek, restaurants});
+  
   const response = {
     statusCode: 200,
     body: html,
