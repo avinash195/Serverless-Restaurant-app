a7eaad8f10fa496d6d0e1a414390f867d913aaa0
Avinash Yadav <avinash@Avinashs-MacBook-Air.local>
2018-10-24 16:37:15 +0530

    video 2.7

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,31 +1,37 @@
 'use strict';
 
-const co =require('co');
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
+const co = require("co");
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
 const Mustache = require('mustache');
 const http = require('superagent-promise')(require('superagent'), Promise);
 const aws4 = require('aws4');
 const URL = require('url');
 
-
 const restaurantsApiRoot = process.env.restaurants_api;
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const awsRegion = process.env.AWS_REGION;
+const cognitoUserPoolId = process.env.cognito_user_pool_id;
+const cognitoIdentityPoolId = process.env.cognito_identity_pool_id;
+const cognitoClientId = process.env.cognito_client_id;
+
 var html;
 
 function* loadHtml() {
   if (!html) {
     html = yield fs.readFileAsync('static/index.html', 'utf-8');
   }
-  return yield fs.readFileAsync('static/index.html', 'utf-8'); 
+
+  return html;
 }
 
 function* getRestaurants() {
   let url = URL.parse(restaurantsApiRoot);
   let opts = {
-    host: url.hostname,
+    host: url.hostname, 
     path: url.pathname
-  }
+  };
 
   aws4.sign(opts);
 
@@ -38,11 +44,20 @@ function* getRestaurants() {
   ).body;
 }
 
-module.exports.handler = co.wrap(function* (event, context, cb) {
+module.exports.handler = co.wrap(function* (event, context, callback) {
   let template = yield loadHtml();
   let restaurants = yield getRestaurants();
-  let daysOfWeek = days[new Date().getDay()];
-  let html = Mustache.render(template, { daysOfWeek, restaurants});
+  let dayOfWeek = days[new Date().getDay()];
+  let view = { 
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoIdentityPoolId,
+    cognitoClientId,
+    dayOfWeek, 
+    restaurants,
+    searchUrl: `${restaurantsApiRoot}/search`
+  };
+  let html = Mustache.render(template, view);    
 
   const response = {
     statusCode: 200,
@@ -52,5 +67,5 @@ module.exports.handler = co.wrap(function* (event, context, cb) {
     }
   };
 
-  cb(null, response);
+  callback(null, response);
 });
