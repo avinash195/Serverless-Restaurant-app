26e0b40fff6c6a23c37f353371064608c98103b9
Avinash Yadav <avinash@Avinashs-MacBook-Air.local>
2018-10-25 11:26:31 +0530

    integration test done successfully video 3.2

---
+++ tests/steps/init.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const co       = require('co');
+const Promise  = require('bluebird');
+const awscred  = Promise.promisifyAll(require('awscred'));
+
+let initialized = false;
+
+let init = co.wrap(function* () {
+  if (initialized) {
+    return;
+  }
+
+  process.env.restaurants_api      = "https://dnz64qvy3f.execute-api.us-east-1.amazonaws.com/dev/restaurants";
+  process.env.restaurants_table    = "restaurants";
+  process.env.AWS_REGION           = "us-east-1";
+  process.env.cognito_user_pool_id = "us-east-1_WMrhCsQk3";
+  process.env.cognito_client_id    = "5at8tl50h67o5t8fmejqaokcqd";
+  
+  let cred = yield awscred.loadAsync();
+  
+  process.env.AWS_ACCESS_KEY_ID     = cred.credentials.accessKeyId;
+  process.env.AWS_SECRET_ACCESS_KEY = cred.credentials.secretAccessKey;
+
+  console.log('AWS credential loaded');
+
+  initialized = true;
+});
+
+module.exports.init = init;
